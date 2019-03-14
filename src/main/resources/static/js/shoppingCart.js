8abb265d3162d6cfdd8f82fdb92c87d5d21b5509
Neddy1995 <xiechao.neddy@foxmail.com>
2019-03-14 23:06:21 +0800

    购物车界面头部和点击返回首页的界面_20190314

---
+++ src/main/resources/static/js/shoppingCart.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //加载头部代码
+    $('.top-html').load('./static/html/tophtml.html');
+
+    // 加载底部代码
+    $('.bottom-html').load('./static/html/bottomhtml.html');
+
+
+
+});
